8f983cff9ce37421d3ff86b0e7cf6afb263dc177
lovespr1ng <lllovespr1ng@gmail.com>
2026-05-21 00:56:13 +0300

    Initial Commit

---
+++ TideFlow/TideFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TideFlowApp.swift
+//  TideFlow
+//
+//  Created by Данил Ротко on 21.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct TideFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
